b77b6be3408f899af91b75840d34e9f3fb1f37c4
Simclem <simon.clement98@yahoo.com>
2017-05-07 23:59:57 +0200

    Ajout du premium resailer

--- Javascript/formcontrol.js
+++ Javascript/formcontrol.js
@@ -8,3 +8,18 @@ function CheckPassword(password, toCheckPassword)
         toCheckPassword.value ="";
     }
 }
+
+
+   function isEmail(myVar){
+     // La 1ère étape consiste à définir l'expression régulière d'une adresse email
+     var regEmail = new RegExp('^[0-9a-z._-]+@{1}[0-9a-z.-]{2,}[.]{1}[a-z]{2,5}$','i');
+
+     return regEmail.test(myVar.value);
+   }
+
+   var emails = new Array('adressemail@gmail','adresse@mel.fr','adr@fr.com.eu');
+
+   for(var e = 0; e < emails.length; e++){
+     console.log('Test sur '+ emails[e] +' : '+ isEmail(emails[e]) +' < br /> ');
+   }
+   
