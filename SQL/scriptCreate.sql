51d0d28b4378557350b86f138905cdbe7ea89dd6
Simclem <simon.clement98@yahoo.com>
2017-05-11 21:03:58 +0200

    Ajout de scripts permettant l'ajout des users admins

--- SQL/scriptCreate.sql
+++ SQL/scriptCreate.sql
@@ -13,3 +13,15 @@ CREATE TABLE `users` (
  `Admin` tinyint(4) NOT NULL,
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+
+
+--Script d'insertion de donnee
+    -- insertion des admins
+
+INSERT INTO `users`(`id`, `Name`, `Surname`, `Phone`, `Login`, `Password`, `Email`, `Admin`) VALUES (NULL,"Ella","Witenberg","0600000000","EWit","123","e.witten@teatime.com",true)
+
+INSERT INTO `users`(`id`, `Name`, `Surname`, `Phone`, `Login`, `Password`, `Email`, `Admin`) VALUES (NULL,"Clement","Simon","0600000001","CSim","456","c.sim@teatime.com",true)
+
+    --insertion des utilisaterus normaux
+    
