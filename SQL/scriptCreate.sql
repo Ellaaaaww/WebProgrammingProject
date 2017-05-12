d8d9126a18a6f86122c3af528abfe3683ad914ea
Simclem <simon.clement98@yahoo.com>
2017-05-12 15:50:38 +0200

    Ajout de ligne dans le script, et ajout de script php permettant l'insertion d users

--- SQL/scriptCreate.sql
+++ SQL/scriptCreate.sql
@@ -13,7 +13,17 @@ CREATE TABLE `users` (
  `Admin` tinyint(4) NOT NULL,
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
+--Script création table produits
 
+
+CREATE TABLE `product` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `name` varchar(50) NOT NULL,
+ `link` varchar(50) NOT NULL,
+ `price` int(11) NOT NULL,
+ `stock` int(11) NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
 
 
 --Script d'insertion de donnee
@@ -23,5 +33,8 @@ INSERT INTO `users`(`id`, `Name`, `Surname`, `Phone`, `Login`, `Password`, `Emai
 
 INSERT INTO `users`(`id`, `Name`, `Surname`, `Phone`, `Login`, `Password`, `Email`, `Admin`) VALUES (NULL,"Clement","Simon","0600000001","CSim","456","c.sim@teatime.com",true)
 
-    --insertion des utilisaterus normaux
+    --insertion des utilisateurs normaux
     
+INSERT INTO `users`(`id`, `Name`, `Surname`, `Phone`, `Login`, `Password`, `Email`, `Admin`) VALUES (NULL,"Jean","Onche","0600000002","J_onche","789","jonche@jvc.com",false)
+
+
