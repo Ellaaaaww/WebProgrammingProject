39978574e515e9ba33fc73a179e3a097fdf1fb76
Ella <ella.witenberg1@gmail.com>
2017-05-12 20:25:57 +0200

    mise en place du script de reset

--- PHP/dbFunctions.php
+++ PHP/dbFunctions.php
@@ -15,7 +15,7 @@
         $stmt->bindParam(':mdp', $pass);
         $stmt->bindParam(':email', $email);
         $stmt->execute();
-    } 
+    }
 
 
 
@@ -53,9 +53,27 @@
 
         $stmt = $db->prepare('INSERT INTO `users`(`id`, `Name`, `Surname`, `Phone`, `Login`, `Password`, `Email`, `Admin`) VALUES (NULL,"Jean","Onche","0600000002","J_onche","789","jonche@jvc.com",false);');
         $stmt->execute();
-                            
 
+        $stmt= $db->prepare("DROP TABLE PRODUCTS");
+        $stmt->execute();
 
+        $stmt = $db->prepare("CREATE TABLE `products` (
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+        `name` varchar(50) NOT NULL,
+        `link` varchar(50) NOT NULL,
+        `price` int(11) NOT NULL,
+        `stock` int(11) NOT NULL,
+        PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB  DEFAULT CHARSET=latin1");
+        $stmt->execute();
+        $stmt = $db->prepare('INSERT INTO `products`(`id`, `name`, `link`, `price`, `stock`) VALUES (NULL,"ceylan_tea","CSS/theOne.jpg",15,100)');
+        $stmt->execute();
+
+        $stmt = $db->prepare('INSERT INTO `products`(`id`, `name`, `link`, `price`, `stock`) VALUES (NULL,"white_tea","CSS/theTwo",15,100)');
+        $stmt->execute();
+
+        $stmt = $db->prepare('INSERT INTO `products`(`id`, `name`, `link`, `price`, `stock`) VALUES (NULL,"jasmin_tea","CSS/theThree",15,100)');
+        $stmt->execute();
 
 
     }
