e6a6077d5db6aba59364759e4c5f32a8b2321065
Simclem <simon.clement98@yahoo.com>
2017-05-18 21:13:58 +0200

    Passage de html à php

--- premium.php
+++ premium.php
@@ -1,3 +1,7 @@
+<?php
+  session_start();
+?>
+
 <!DOCTYPE html>
 <html>
    <head>
@@ -16,9 +20,23 @@
       <header>
       <div class="row" >
          <div class="col-md-3 col-md-offset-9">
-            <button type="button" class="btn btn-default " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onclick="location.href='SignIn.html'"> Sign in</button>
-            <button type="button" class="btn btn-default " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onclick="location.href='SignUp.html'"> Sign up</button>
-            <a href="basket.html"><i class="fa fa-shopping-basket" aria-hidden="true" style = "color : yellow;"></i></a>
+             <?php
+                if (empty($_SESSION['Surname']))
+                {
+                    ?>
+                        <button type="button" class="btn btn-default " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onclick="location.href='SignIn.html'"> Sign in</button>
+                        <button type="button" class="btn btn-default " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onclick="location.href='SignUp.html'"> Sign up</button>
+                        <a href="basket.html"><i class="fa fa-shopping-basket" aria-hidden="true" style = "color : yellow;"></i></a>
+
+                    <?php
+                }
+                else
+                {
+                    ?>
+                        <button type="button" class="btn btn-default " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onclick="location.href='logout.php'"> Logout</button>
+                    <?php
+                }
+             ?>
          </div>
       </div>
       <div class="row">
@@ -49,6 +67,19 @@
                            <li class="collapse navbar-collapse"><a href="myAccount.html"> My Account<span class="sr-only">(current)</span></a></li>
                            <li><a href="advices.html"> Advices </a></li>
                            <li><a href="listProduct.html"> Our products </a></li>
+                           <?php
+                            if (!empty($_SESSION['Surname']))
+                            {
+                                if (intVal($_SESSION['Admin']) == 1)
+                                {
+                                    ?>
+                                     <li><a href="database.php"> Database Administration </a></li>
+                                    <?php
+                                }
+
+                            }
+
+                            ?>
                         </div>
                      </div>
                      <!-- /.navbar-collapse -->
@@ -63,7 +94,7 @@
       <!--End header-->
       <!--Begin core-->
       <div class="row">
-        <h1 class="namePage">Our location</h1>
+        <h1 class="namePage">Premium sailer</h1>
         <div class="row">
             <div class="centerText">A premium resailer is situated at 777 boulevard Robert-Bourassa in Montréal</div>
         </div>
@@ -81,16 +112,16 @@
                   <div class = "col-md-4">
                      <h3 class = "footer">The house</h3>
                      <br />
-                     <a href = "Story.html" >Story</a>
+                     <a href = "Story.php" >Story</a>
                      <br />
                      <a href="mailto:adresse@serveur.com">Contact us</a>
                   </div>
                   <div class = "col-md-4">
                      <h3 class = "footer">Find us</h3>
                      <br />
-                     <a href = "ShopLocation.html">Our shop</a>
+                     <a href = "ShopLocation.php">Our shop</a>
                      <br />
-                     <a href = "premium.html" >Premium resailer</a>
+                     <a href = "premium.php" >Premium resailer</a>
                   </div>
                   <div class = "col-md-4">
                      <h3 class = "footer">Social network</h3>
