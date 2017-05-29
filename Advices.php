84b140e4c8d7f67a4aac57d43f9b4b9b337c8b20
Ella <ella.witenberg1@gmail.com>
2017-05-30 00:52:27 +0200

    modif advice

--- Advices.php
+++ Advices.php
@@ -6,7 +6,49 @@
          <div class="container-fluid" style="color : white">
             <div class="col-md-4 col-md-offset-4">
                <img src = "CSS/theJapan1.jpg" >
-               <label>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dapibus elit vitae convallis lacinia. Sed eget tortor elementum nibh vulputate bibendum id luctus quam. Vestibulum at pellentesque quam. Sed pharetra augue sit amet ultrices lacinia.</label>
+               <label> <h2> Water</h2><br />
+                 <div>
+                 <div>Always use freshly drawn, cold water to prepare tea.
+                   <br /> Purified and spring water are best because they are relatively free from pollutants and other substances that can dramatically alter tea's taste. On the other hand, it is preferable for the water to have some natural minerals content, as they may enhance the tea's flavor.
+                   <br /> For this reason, distilled water should be avoided since the lack of minerals will leave the tea tasting flat.
+                   <br /> Similarly, one should never use pre-heated water-as from the faucet, for instance. This kind of water has mostly likely been overheated, thereby losing oxygen content while picking up possibly harmful substances from the water pipes. </div><br /><br />
+
+                 <div>Traditionally, in Asia, water is always brought to gentle boil before one prepares tea. Boiling water eliminates many harmful germs and bacteria.
+                    <br />Though water quality has improved vastly, boiling in the water in this fashion can help to bring out tea's flavor.
+                    <br /> The water should be heated until a steady stream of air bubbles gently rise to the surface. At this point, the water is sufficiently heated and also has a preferable oxygen content.
+                   <br /> In contrast, using water that has been held at a fierce, rolling boil can leave tea tasting dull and flat.</div><br /><br />
+
+                 <h2>Temperature</h2><br />
+                  <div>Temperature is critical in making good tea. The popular conception that boiling water can be used on any kind of tea is actually incorrect. In fact, each variety of tea,
+                   from green to black, needs to prepared at a different range of temperature. Because of its more delicate processing, green tea often needs a cooler temperature to bring out the right flavor.
+                   <br /> Conversely, black tea, which has been fully oxidized, needs much hotter water to bring out its characteristic baked sweetness.</div><br /><br />
+
+                   <h2>Duration OF Infusion</h2><br />
+                   <div>Similar to water temperature, different kinds of tea need to brewed for different lengths of time. Generally, delicate teas such as green tea need to be brewed for shorter
+                  <br />times, while heartier black teas and earthy, fermented pu-erh teas benefit from longer infusions. Of course, the duration of the infusion varies with culture and personal preference.</div><br /><br />
+
+                  <h2>Material Of Teapot</h2><br />
+                  <div>While seemingly unimportant at first, the material of the teapot being used also affects the quality of the infusion. When considering a teapot, it is important to consider
+                    the variety of tea and the temperature at which it is prepared.
+                    <br /> Materials like iron or Chinese yixing ware are excellent at retaining heat over long periods of time, while
+                    glass or porcelain are more likely to release that heat.
+                    <br /> Therefore, iron and similarly heavy materials are better for teas that need to be prepared at high temperatures,
+                    such as black tea or yixing.
+                    <br /> A teapot made from iron would keep the water hot enough to extract the teas full flavor. Green and white teas, on the other hand, needs a
+                    vessel that stays cooler, such as porcelain.</div><br /><br />
+
+                    <h2>General Guidelines</h2><br />
+                      <div>The following instructions will allow you to make a good cup of any kind of tea. Also included are suggestions for amount of tea leaves, time of infusion, water temperature, and material of teapot for the general varieties of tea. Of course, there are more detailed instructions for any kind of tea.</div><br />
+
+<div>
+1. Bring freshly drawn, cold water to a boil in a kettle.<br />
+2. When water is at a gentle boil, remove heat.<br />
+3. Pour hot water into teapot and teacups and pour off. By warming the cups in this way, the water temperature will be more consistent.<br />
+4. Add the proper amount of tea leaves per person to the pot.<br />
+5. Allow water to cool to the proper temperature, if necessary, and pour over the tea leaves.<br />
+6. Steep for the proper length of time.<br />
+7. Strain completely into another teapot or directly into the serving cups.<br /></div></label>
+</div>
                <div class="row">
                   <iframe width="560" height="315" src="https://www.youtube.com/embed/GgGqBKGQwc4" frameborder="0" allowfullscreen></iframe>
                </div>
